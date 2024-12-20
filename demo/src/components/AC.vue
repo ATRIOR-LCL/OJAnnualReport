45ca6ab956f33e57ee9ef275f783aab271cc37e6
SDUTAIer <sdutwujinhao@gmail.com>
2024-12-20 17:05:44 +0800

    11

--- demo/src/components/AC.vue
+++ demo/src/components/AC.vue
@@ -7,9 +7,19 @@
         <div class="sts">你的刷题数傲立群雄， 超过了OJ 00%的用户</div>
     </div>
     <div class="acstar">
-        <img src="../assets/img/star.png" alt="" class="star2">
-        <img src="../assets/img/star.png" alt="" class="star1">
-        <img src="../assets/img/star.png" alt="" class="star3">
+        <div class="stzx">
+            <img src="../assets/img/qq.png" alt="">
+            <span class="rank2">atrior</span>
+            <img src="../assets/img/qq.png" alt="">
+            <span class="rank1">atrior</span>
+            <img src="../assets/img/qq.png" alt="">
+            <span class="rank3">atrior</span>
+        </div>
+        <img ref="star2" src="../assets/img/star.png" alt="" class="star2">
+        <img ref="star1" src="../assets/img/star.png" alt="" class="star1">
+        <img ref="star3" src="../assets/img/star.png" alt="" class="star3">
+        <img src="../assets/img/star.png" alt="" class="star4">
+        <img src="../assets/img/star.png" alt="" class="star5">
     </div>
 </div>
 </section>
@@ -17,11 +27,51 @@
 
 <script setup>
 import{ref, onMounted} from 'vue'
+const star1 = ref(null)
+const star2 = ref(null)
+const star3 = ref(null)
+
 onMounted(()=>{
+    const txtanim = new IntersectionObserver(lists=>{
+        lists.forEach(list=>{
+            if(list.isIntersecting){
+                list.target.classList.add('active')
+                star1.value.style.animationPlayState = 'running'
+                star2.value.style.animationPlayState = 'running'
+                star3.value.style.animationPlayState = 'running'
+            }else{
+                list.target.classList.remove('active')
+            }
+        })
+    })
+    const txt = document.querySelector('.actest')
+    txtanim.observe(txt)
+
+    const acstarmove  = new IntersectionObserver(lists=>{
+        lists.forEach(list=>{
+            if(list.isIntersecting){
+                list.target.classList.remove('move')
+            }else{
+                list.target.classList.add('move')
+            }
+        })
+    })
+
+    const acst = document.querySelector('.acstar')
+    acstarmove.observe(acst)
 
 })
 </script>
 
 <style scoped>
 @import url('../assets/ac.css');
+.active{
+    opacity: 1;
+    filter: blur(0);
+    transform: translateX(0);
+}
+
+.move{
+    opacity: 0;
+}
 </style>
