a1976fd4898c37d9fb5cfb75b8c29fa7be7879fc
SDUTAIer <sdutwujinhao@gmail.com>
2024-12-20 21:10:27 +0800

    22

--- demo/src/components/Hard.vue
+++ demo/src/components/Hard.vue
@@ -4,7 +4,7 @@
             <div class="hardtxt">
                 <span>还记得</span><span>xxxx</span><span>这道题吗</span><span>你提交了整整00次才过</span>
             </div>
-            <div class="hard_slogan">那份喜悦背后的坚韧，一定也陪你走到了今天
+            <div class="hard_slogan">那份喜悦背后的坚韧,一定也陪你走到了今天
                 <img src="../assets/img/liuyinh.webp" alt="" class="liuying">
             </div>
             <div class="hdsea"></div>
