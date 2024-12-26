daf97916eaf3a3558846e99833bdd6f5c82ddbaf
SDUTAIer <sdutwujinhao@gmail.com>
2024-12-26 16:04:35 +0800

    11

--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -1,21 +1,21 @@
-import Login from '@/views/Login.vue';
-import Main from '@/views/Main.vue';
-import {createRouter, createWebHistory} from 'vue-router'
+import Login from "@/views/Login.vue";
+import Main from "@/views/Main.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-const router=createRouter({
-    history: createWebHistory(),
-    routes:[
-        {
-            path: '/',
-            name: 'main',
-            component: Main
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: Login
-        }
-    ]
-})
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "main",
+      component: Main,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+  ],
+});
 
-export default router
+export default router;
